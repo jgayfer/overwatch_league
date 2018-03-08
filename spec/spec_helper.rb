04ca53b6e9f6277e1ec0173106f995b745d33081
James Gayfer <gayfer.james@gmail.com>
2018-03-07 20:38:01 -0800

    Setup testing framework

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,9 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'overwatch_league'
+require 'webmock/rspec'
+
+RSpec.configure do |config|
+  config.before(:each) do
+    stub_request(:any, /api.overwatchleague.com/)
+  end
+end
