4ca22919c5334d149ac17243dfa975ca1cdae402
James Gayfer <gayfer.james@gmail.com>
2018-03-31 17:37:13 -0700

    Change from require to require_relative

--- lib/overwatch_league.rb
+++ lib/overwatch_league.rb
@@ -1,9 +1,5 @@
-require "overwatch_league/version"
-require "overwatch_league/url"
-require "overwatch_league/request"
-require "overwatch_league/api"
-require "overwatch_league/response_error"
-
-module OverwatchLeague
-  # Your code goes here...
-end
+require_relative "overwatch_league/version"
+require_relative "overwatch_league/url"
+require_relative "overwatch_league/request"
+require_relative "overwatch_league/api"
+require_relative "overwatch_league/response_error"
