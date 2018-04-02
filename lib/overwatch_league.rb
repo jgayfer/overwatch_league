26e4e29971c6354e756e82b70738697ead1b4624
James Gayfer <gayfer.james@gmail.com>
2018-04-02 09:12:37 -0700

    Add alias for Client.new

--- lib/overwatch_league.rb
+++ lib/overwatch_league.rb
@@ -4,3 +4,11 @@ require_relative "overwatch_league/request"
 require_relative "overwatch_league/api"
 require_relative "overwatch_league/response_error"
 require_relative "overwatch_league/client"
+
+module OverwatchLeague
+  class << self
+    def new
+      Client.new
+    end
+  end
+end
